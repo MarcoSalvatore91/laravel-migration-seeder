2f33df6be25f971af7f7721952a31c3aa364677d
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-18 15:25:27 +0100

    created parameter trains in create-trains_table

--- database/migrations/2022_03_18_140439_create_trains_table.php
+++ database/migrations/2022_03_18_140439_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('laravel_trains_db', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->string('departure_time', 50);
+            $table->string('arrival_time', 50);
+            $table->char('train_code', 8);
+            $table->tinyInteger('carriages');
+            $table->boolean('in_time')->default(1);
+            $table->boolean('deleted')->default(0);
             $table->timestamps();
         });
     }
